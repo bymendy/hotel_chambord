901d87cb18cb9639d2fb99ea1014dfc3c4f475af
barrymendy <barrymendy@gmail.com>
2023-01-30 14:31:43 +0100

    Suite du dev projet chambord

--- js/app-burger.js
+++ js/app-burger.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-    $("#hamburger").on("click", function(){
-      $(".navbar-brand").toggleClass("active");
-    });
-  });
+// $(document).ready(function(){
+//     $("hamburger").on("click", function(){
+//       $(".navbar-brand").toggleClass("active");
+//     });
+//   });
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-burger');
+
+hamburger.addEventListener('click', () => {
+  menu-burger.classList.toggle('active');
+});
