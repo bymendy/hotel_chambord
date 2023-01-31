7c06af97cabf6b8adcfa09ce3d4e8fce2e4a1b94
barrymendy <barrymendy@gmail.com>
2023-01-31 01:11:35 +0100

    suite du projet

--- js/app-burger.js
+++ js/app-burger.js
@@ -1,11 +1,26 @@
-// $(document).ready(function(){
-//     $("hamburger").on("click", function(){
-//       $(".navbar-brand").toggleClass("active");
-//     });
-//   });
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu-burger');
+const nav = document.querySelector('navbar-brand');
+const li = document.querySelectorAll('.Nav a');
+// console.log(nav);
 
-hamburger.addEventListener('click', () => {
-  menu-burger.classList.toggle('active');
-});
+//Check every link
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener('click', function(e){
+    
+    //If list has nested list
+    if(this.nextElementSibling.className.indexOf('hamburger') !== -1) {
+      // Prevent anchor
+      e.preventDefault();
+      
+      //Hide previous nav
+      nav.lastElementChild.classList.remove('isActive');
+      
+      //Clone and append to bottom
+      const clone = this.nextElementSibling.cloneNode(true);
+      nav.appendChild(clone);
+      
+      //Make it visible
+      nav.lastElementChild.classList.add('isActive');
+    }
+  });
+}
+                         
