2b3e6c9b6d7420bebe0bb8bbe42f20d194ca1019
barrymendy <barrymendy@gmail.com>
2023-02-04 01:58:30 +0100

    commit du soir

--- js/app-burger.js
+++ js/app-burger.js
@@ -1,26 +1,9 @@
-const nav = document.querySelector('navbar-brand');
-const li = document.querySelectorAll('.Nav a');
-// console.log(nav);
-
-//Check every link
-for (var i = 0; i < li.length; i++) {
-  li[i].addEventListener('click', function(e){
-    
-    //If list has nested list
-    if(this.nextElementSibling.className.indexOf('hamburger') !== -1) {
-      // Prevent anchor
-      e.preventDefault();
-      
-      //Hide previous nav
-      nav.lastElementChild.classList.remove('isActive');
-      
-      //Clone and append to bottom
-      const clone = this.nextElementSibling.cloneNode(true);
-      nav.appendChild(clone);
-      
-      //Make it visible
-      nav.lastElementChild.classList.add('isActive');
-    }
-  });
-}
-                         
+$('menu a').click(function(){
+ 
+  $('.trigger').toggle();
+  $('.menu').toggleClass('round');
+  $('.close').toggle();
+  $('.drop-down').toggleClass('.down');
+  
+ 
+});                    
